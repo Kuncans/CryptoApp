abdca12dbb50efbbb50275de01d1a6cc4e3f7558
Kuncans <akafauna@gmail.com>
2022-01-31 23:24:58 +0000

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Duncan Kent on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
