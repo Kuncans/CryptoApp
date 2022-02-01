ffc646d1f0d2423a5be4b541e8c7278a66482c3b
Kuncans <akafauna@gmail.com>
2022-02-01 14:47:33 +0000

    dynamic coin lists and column headings

---
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewModel.swift
+//  CryptoApp
+//
+//  Created by Duncan Kent on 01/02/2022.
+//
+
+import Foundation
+
+final class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}
